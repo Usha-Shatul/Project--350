c2de56c02c2f11a1e44efb12c8283b7a02119f1b
Md Mahabub Rana Saikat <mahabubranasaikat@gmail.com>
2026-05-02 14:09:08 +0600

    Backend_fixed_by_saikat

---
+++ backend/routes/registrationRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const registrationController = require('../controllers/registrationController');
+const { verifyToken } = require('../middleware/auth');
+
+router.post('/', verifyToken, registrationController.createRegistration);
+router.get('/', verifyToken, registrationController.getRegistrations);
+router.get('/:id', verifyToken, registrationController.getRegistration);
+router.put('/:id', verifyToken, registrationController.updateRegistration);
+router.post('/:id/register', verifyToken, registrationController.registerFree);
+router.put('/:id/close', verifyToken, registrationController.closeRegistration);
+router.delete('/:id', verifyToken, registrationController.deleteRegistration);
+router.get('/:id/participants', verifyToken, registrationController.getParticipants);
+router.get('/:id/payments', verifyToken, registrationController.getPayments);
+
+module.exports = router;
